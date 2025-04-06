a57916456a3f19ca0e9a2961065fa49dc4ae309c
Darina Khalina <darina.khalina@namecheap.com>
2025-04-06 21:36:17 +0300

    add User model

--- src/database/models.py
+++ src/database/models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Integer, String, Date, Column
-from sqlalchemy.orm import DeclarativeBase
+from xmlrpc.client import DateTime, Boolean
+from sqlalchemy import Integer, String, Date, Column, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, relationship
+from datetime import datetime
 
 
 class Base(DeclarativeBase):
@@ -15,3 +17,17 @@ class Contact(Base):
     email = Column(String(200), nullable=False)
     phone = Column(String(50), nullable=False)
     birthday = Column(Date, nullable=False)
+    user_id = Column(ForeignKey("users.id", ondelete="CASCADE"), default=None)
+    user = relationship("User", backref="contacts")
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.now())
+    avatar = Column(String(255), nullable=True)
+    confirmed = Column(Boolean, default=False, nullable=True)
