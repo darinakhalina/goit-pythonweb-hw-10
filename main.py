11e30547f761a96332c914a6ff6fb748954eae23
Darina Khalina <darina.khalina@namecheap.com>
2025-04-06 22:10:23 +0300

    add cors

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.api import contacts, utils
 
 from src.conf.config import settings
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(utils.router, prefix="/api")
 app.include_router(contacts.router, prefix="/api")
